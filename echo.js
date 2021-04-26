32a7ff9f3709af539dcc71055cc64c83ea50443c
Nick10062 <46979113+Nick10062@users.noreply.github.com>
2021-04-26 11:52:01 -0400

    Added echo files

---
+++ echo.js
@@ -0,0 +1,72 @@
+import EchoElement from './echoElement.js';
+
+class Echo {
+
+    static async initializeElements() {
+        let elements = await this.getEchoElements();
+
+        for(let e of elements) {
+            let elementData = this.getElementData(e);
+
+            this.createCustomElementClass(elementData)
+        }
+    }
+    
+    static createCustomElementClass(elementData) {
+        const customElementClass = class CustomElement extends EchoElement {
+            static get observedAttributes() {
+                return elementData.attributes;
+            }
+
+            constructor() {
+                super();
+
+                this.elementHTML = elementData.elementHTML;
+            }
+        }
+
+        this.createEchoElement(elementData.className, customElementClass);
+    }
+
+    static createEchoElement(tagName, elementClass) {
+        // Get the class name in slug case
+        tagName = tagName.replace(/([A-Z])/g, "-$1").toLowerCase().substring(1);
+
+        if(tagName.indexOf("echo-") === -1) tagName = `echo-${tagName}`;
+
+        customElements.define(tagName, elementClass);
+    }
+
+    static async getEchoElements() {
+        let elements;
+
+        await fetch('./echoElements.html')
+            .then(response => response.text())
+            .then(data => elements = data.split(/(?=<echo-element .+>)/));
+
+        return elements;
+    }
+
+    static getElementData(element) {
+        const template = document.createElement('template');
+        template.innerHTML = element;
+
+        let attributes = element.substring(0, element
+            .search(/( ?)+>/))
+            .replace('<echo-element', '')
+            .replace(/echoname=("|')\w+("|')/, '')
+            .split(/ /)
+            .filter(x => x.length > 0)
+            .map(x => x.substring(0, x.indexOf('='))
+        );
+
+        return {
+            className: template.content.firstChild.getAttribute('echoname').replace(/^\w/, c => c.toUpperCase()),
+            attributes: attributes,
+            elementHTML: template.content.firstChild.innerHTML
+        };
+    }
+
+}
+
+Echo.initializeElements();
---
+++ echoElement.js
@@ -0,0 +1,65 @@
+export default class EchoElement extends HTMLElement {
+    
+    static get observedAttributes() {
+        return [];
+    }
+
+    constructor() {
+        super();
+
+        this.elementHTML = '<div></div>';
+    }
+
+
+    attributeChangedCallback(name, oldVal, newVal) {
+        this.update();
+    }
+
+    async connectedCallback() {
+        this.bindAccessors();
+        this.update();
+    }
+
+    bindAccessors() {
+        const attributes = this.constructor.observedAttributes;
+
+        if(attributes === null) throw new TypeError(`attributes is null`);
+        if(attributes !== undefined) {
+            for(let i of attributes) {
+                this.setAttribute(i, this.getAttribute(i));
+
+                this.__defineGetter__(i, () => {
+                    return this.getAttribute(i);
+                });
+
+                this.__defineSetter__(i, (val) => {
+                    return this.setAttribute(i, val);
+                });
+            }   
+        }
+
+    }
+
+    setDynamicData() {
+        const attributes = this.constructor.observedAttributes;
+        let dynamicHTML = this.elementHTML;
+
+        for(let i of attributes) {
+            // Set getters by looking for {varname}
+            dynamicHTML = dynamicHTML.replaceAll(`{${i}}`, this.getAttribute(i));
+            // Set setters by looking for this.varname
+            dynamicHTML = dynamicHTML.replaceAll(`this.${i}`, `this.closest('${this.tagName.toLowerCase()}').${i}`);
+        }
+
+        return dynamicHTML;
+    }
+
+    update() {
+        this.innerHTML = `
+            <div>
+                ${this.setDynamicData()}
+            </div>
+        `;
+    }
+
+}
